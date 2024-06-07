bf2ea433e07b61420e55729fd6caa95e0c8dc29b
MKO <kiril309274@gmail.com>
2024-06-07 11:44:40 +0200

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
+      {/*<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>*/}
       <body className={inter.className}>{children}</body>
-    </html>
+      </html>
   );
 }
