10ebecdfb74d05a49999ce0d5947f751e5c53807
Maksym Habinskyi <mgabin348@gmail.com>
2024-06-18 22:05:58 +0200

    navbar fix

--- src/components/header/navbar/Navbar.tsx
+++ src/components/header/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Link from "next/link";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
 
     return (
-        <nav className="navbar">
+        <nav className={styles.navbar}>
             <Link href="#home" className="active">Home</Link>
             <Link href="#about">About</Link>
             <Link href="#education">Education</Link>
