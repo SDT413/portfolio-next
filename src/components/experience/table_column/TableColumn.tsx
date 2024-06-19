4375815a6e9803df78b062382a3ba496d7d3125d
MKO <kiril309274@gmail.com>
2024-06-19 20:30:03 +0200

    education desktop ready

--- src/components/experience/table_column/TableColumn.tsx
+++ src/components/experience/table_column/TableColumn.tsx
@@ -16,8 +16,22 @@ const TableColumn: FC<ITableColumnProps> = ({heading, data}) => {
                 {heading}
             </div>
             {
-                data.map((data,index) => (
-                    <TableSection key={index} date={data.date} heading={data.heading} description={data.description}/>
+                data.map((item,index) => (
+                    data.length - 1 !== index ?
+                        <div className={styles.elements_wrapper} key={index}>
+                            <span className={styles.elements}>
+                            <span className={styles.circle}/>
+                            <span className={styles.line}/>
+                            </span>
+                            <TableSection date={item.date} heading={item.heading} description={item.description}/>
+                        </div>
+                        :
+                        <div className={styles.elements_wrapper} key={index}>
+                             <span className={styles.elements}>
+                            <span className={styles.circle}/>
+                             </span>
+                            <TableSection date={item.date} heading={item.heading} description={item.description}/>
+                        </div>
                 ))
             }
         </div>
