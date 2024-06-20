9cc3159491abd7a14771afe69dc66aad62e6403a
Maksym Habinskyi <mgabin348@gmail.com>
2024-06-20 20:10:25 +0200

    logo added

--- src/components/header/logo/Logo.tsx
+++ src/components/header/logo/Logo.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./Logo.module.css";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "./assets/logo.png";
 
 const Logo = () => {
   return (
     <div className={styles.container}>
       <Link href="#home" className={styles.logo}>
-        Kirill.
+        <Image className={styles.logo_image} src={logo} alt={""}></Image>
       </Link>
       <div className="bx bx-menu" id={styles.menu_icon}></div>
     </div>
