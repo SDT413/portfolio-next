0d8d0f40bd591c6ce69dc597f4c9341e26c42b05
Maksym Habinskyi <mgabin348@gmail.com>
2024-06-20 21:31:28 +0200

    logo fix

--- src/components/header/logo/Logo.tsx
+++ src/components/header/logo/Logo.tsx
@@ -7,7 +7,7 @@ import logo from "./assets/logo.png";
 const Logo = () => {
   return (
     <div className={styles.container}>
-      <Link href="#home" className={styles.logo}>
+      <Link href="#home" className={styles.logo} >
         <Image className={styles.logo_image} src={logo} alt={""}></Image>
       </Link>
       <div className="bx bx-menu" id={styles.menu_icon}></div>
