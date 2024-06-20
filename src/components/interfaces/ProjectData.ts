cd243f169c9e44b3a2459d06ec1e50d7463762e2
MKO <kiril309274@gmail.com>
2024-06-20 21:55:42 +0200

    changes

--- src/components/interfaces/ProjectData.ts
+++ src/components/interfaces/ProjectData.ts
@@ -1,7 +1,10 @@
+import {FC, SVGProps} from "react";
+
 export interface ProjectData {
     title: string;
     description: string;
     technologies: string[];
+    svg: FC<SVGProps<SVGSVGElement>>;
     githubLink?: string;
     liveDemoLink?: string;
 }
