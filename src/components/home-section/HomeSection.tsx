38775cbb9293067f5f3c6665889c7789ab12866e
Maksym Habinskyi <mgabin348@gmail.com>
2024-06-30 18:06:27 +0200

    header changed; tech stack items added

--- src/components/home-section/HomeSection.tsx
+++ src/components/home-section/HomeSection.tsx
@@ -1,4 +1,4 @@
-import {FC, useEffect, useRef} from "react";
+import { FC, useEffect, useRef } from "react";
 import styles from "./HomeSection.module.css";
 import { useActions } from "@/hooks/useActions";
 import HTMLSVG from "./assets/svgs/HTMLSVG";
@@ -10,7 +10,14 @@ import TailwindSVG from "./assets/svgs/TailwindSVG";
 import SCSSSVG from "./assets/svgs/SCSSSVG";
 import NextJSSVG from "./assets/svgs/NextJSSVG";
 import TSSVG from "./assets/svgs/TSSVG";
-import {Observer} from "@/utils/observer";
+import { Observer } from "@/utils/observer";
+import SQLSVG from "./assets/svgs/SQLSVG";
+import SupabaseSVG from "./assets/svgs/SupabaseSVG";
+import MongoDBSVG from "./assets/svgs/MongoDBSVG";
+import DockerSVG from "./assets/svgs/DockerSVG";
+import UnitySVG from "./assets/svgs/UnitySVG";
+import TensorFlowSVG from "./assets/svgs/TensorFlowSVG";
+import JavaSpringSVG from "./assets/svgs/JavaSpringSVG";
 
 const HomeSection: FC = ({}) => {
   const cbRef = Observer("home");
@@ -40,22 +47,39 @@ const HomeSection: FC = ({}) => {
 
       <div className={styles.tech_stack}>
         Tech stack |
-        <span className={styles.tech_stack_item}>
-          <HTMLSVG className={styles.icon} /> <CSSSVG className={styles.icon} />
-        </span>
-        <span className={styles.tech_stack_item}>
-          <JSSVG className={styles.icon} />
-          <TSSVG className={styles.icon} style={{ transform: "scale(0.9)" }} />
-        </span>
-        <span className={styles.tech_stack_item}>
-          <ReactSVG className={styles.icon} />
-          <NextJSSVG className={styles.icon} />
-          <NodeJSSVG className={styles.icon} />
-        </span>
-        <span className={styles.tech_stack_item}>
-          <TailwindSVG className={styles.icon} />
-          <SCSSSVG className={styles.icon} />
-        </span>
+        <div className={styles.tech_stack_items_container}>
+          <span className={styles.tech_stack_item}>
+            <HTMLSVG className={styles.icon} />{" "}
+            <CSSSVG className={styles.icon} />
+          </span>
+          <span className={styles.tech_stack_item}>
+            <JSSVG className={styles.icon} />
+            <TSSVG
+              className={styles.icon}
+              style={{ transform: "scale(0.9)" }}
+            />
+            <TensorFlowSVG className={styles.icon} />
+          </span>
+          <span className={styles.tech_stack_item}>
+            <ReactSVG className={styles.icon} />
+            <NextJSSVG className={styles.icon} />
+            <NodeJSSVG className={styles.icon} />
+            <JavaSpringSVG className={styles.icon} />
+          </span>
+          <span className={styles.tech_stack_item}>
+            <TailwindSVG className={styles.icon} />
+            <SCSSSVG className={styles.icon} />
+          </span>
+          <span className={styles.tech_stack_item}>
+            <SQLSVG className={styles.icon} />
+            <MongoDBSVG className={styles.icon} />
+            <SupabaseSVG className={styles.icon} />
+            <DockerSVG className={styles.icon} />
+          </span>
+          <span className={styles.tech_stack_item}>
+            <UnitySVG className={styles.icon} />
+          </span>
+        </div>
       </div>
       <div className={styles.home_sci}>
         <a href="#" className={styles.sci_link}>
