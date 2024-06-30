66a2562230095f1338965f2ca93282e282dfe08e
MKO <118934540+SDT413@users.noreply.github.com>
2024-06-30 19:19:26 +0200

    home section buttons

--- src/components/home-section/HomeSection.tsx
+++ src/components/home-section/HomeSection.tsx
@@ -1,6 +1,5 @@
-import {FC, useEffect, useRef} from "react";
+import {FC, useState} from "react";
 import styles from "./HomeSection.module.css";
-import { useActions } from "@/hooks/useActions";
 import HTMLSVG from "./assets/svgs/HTMLSVG";
 import CSSSVG from "./assets/svgs/CSSSVG";
 import JSSVG from "./assets/svgs/JSSVG";
@@ -11,64 +10,84 @@ import SCSSSVG from "./assets/svgs/SCSSSVG";
 import NextJSSVG from "./assets/svgs/NextJSSVG";
 import TSSVG from "./assets/svgs/TSSVG";
 import {Observer} from "@/utils/observer";
+import PopupModal from "@/components/home-section/popup-form/PopupModal";
+import ContactForm from "@/components/home-section/popup-form/contact-form/ContactForm";
+import HireForm from "@/components/home-section/popup-form/hire-form/HireForm";
 
 const HomeSection: FC = ({}) => {
   const cbRef = Observer("home");
-  return (
-    <section className={styles.home} id={styles.home} ref={cbRef}>
-      <div className={styles.home_content}>
-        <h1 className={styles.header1}>
-          Hi, I am <span>Kirill Marchenko</span>
-        </h1>
-        <div className={styles.text_animate}>
-          <h3 className={styles.header3}>Fullstack Developer</h3>
-        </div>
-        <p className={styles.paragraph}>
-          I am a student at the University of Prague(PCU). I have 1 year
-          experience with react, wordpress and node.js.
-        </p>
-        <div className={styles.btn_box}>
-          <a href="#" className={styles.btn}>
-            Hire Me
-          </a>
-          <a href="#" className={styles.btn}>
-            Contact Me
-          </a>
-        </div>
-      </div>
-      <div className={styles.home_img}></div>
+  const [showContactForm, setShowContactForm] = useState<boolean>(false);
+  const [showHireForm, setShowHireForm] = useState<boolean>(false);
 
-      <div className={styles.tech_stack}>
-        Tech stack |
-        <span className={styles.tech_stack_item}>
-          <HTMLSVG className={styles.icon} /> <CSSSVG className={styles.icon} />
+  return (
+      <section className={styles.home} id={styles.home} ref={cbRef}>
+          {
+              showHireForm && (
+                    <PopupModal>
+                        <HireForm setShowHireForm={setShowHireForm} />
+                    </PopupModal>
+              )
+          }
+            {
+                showContactForm && (
+                    <PopupModal>
+                        <ContactForm setShowHireForm={setShowContactForm} />
+                    </PopupModal>
+                )
+            }
+        <div className={styles.home_content}>
+          <h1 className={styles.header1}>
+            Hi, I am <span>Kirill Marchenko</span>
+          </h1>
+          <div className={styles.text_animate}>
+            <h3 className={styles.header3}>Fullstack Developer</h3>
+          </div>
+          <p className={styles.paragraph}>
+            I am a student at the University of Prague(PCU). I have 1 year
+            experience with react, wordpress and node.js.
+          </p>
+          <div className={styles.btn_box}>
+            <button className={styles.btn} onClick={() => setShowHireForm(true)}>
+              Hire Me
+            </button>
+              <button className={styles.btn} onClick={() => setShowContactForm(true)}>
+              Contact Me
+            </button>
+          </div>
+        </div>
+        <div className={styles.home_img}></div>
+
+        <div className={styles.tech_stack}>
+          Tech stack |
+          <span className={styles.tech_stack_item}>
+          <HTMLSVG className={styles.icon}/> <CSSSVG className={styles.icon}/>
         </span>
-        <span className={styles.tech_stack_item}>
-          <JSSVG className={styles.icon} />
-          <TSSVG className={styles.icon} style={{ transform: "scale(0.9)" }} />
+          <span className={styles.tech_stack_item}>
+          <JSSVG className={styles.icon}/>
+          <TSSVG className={styles.icon} style={{transform: "scale(0.9)"}}/>
         </span>
-        <span className={styles.tech_stack_item}>
-          <ReactSVG className={styles.icon} />
-          <NextJSSVG className={styles.icon} />
-          <NodeJSSVG className={styles.icon} />
+          <span className={styles.tech_stack_item}>
+          <ReactSVG className={styles.icon}/>
+          <NextJSSVG className={styles.icon}/>
+          <NodeJSSVG className={styles.icon}/>
         </span>
-        <span className={styles.tech_stack_item}>
-          <TailwindSVG className={styles.icon} />
-          <SCSSSVG className={styles.icon} />
+          <span className={styles.tech_stack_item}>
+          <TailwindSVG className={styles.icon}/>
+          <SCSSSVG className={styles.icon}/>
         </span>
-      </div>
-      <div className={styles.home_sci}>
-        <a href="#" className={styles.sci_link}>
-          <i className="bx bxl-facebook"></i>
-        </a>
-        <a href="#" className={styles.sci_link}>
-          <i className="bx bxl-twitter"></i>
-        </a>
-        <a href="#" className={styles.sci_link}>
-          <i className="bx bxl-linkedin"></i>
-        </a>
-      </div>
-    </section>
+        </div>
+        <div className={styles.home_sci}>
+          <a href="#" className={styles.sci_link}>
+            <i className="bx bxl-facebook"></i>
+          </a>
+          <a href="#" className={styles.sci_link}>
+            <i className="bx bxl-twitter"></i>
+          </a>
+          <a href="#" className={styles.sci_link}>
+            <i className="bx bxl-linkedin"></i>
+          </a>
+        </div>
+      </section>
   );
 };
 
---
+++ src/components/home-section/popup-form/contact-form/ContactForm.tsx
@@ -0,0 +1,53 @@
+import React, {FC} from 'react';
+import styles from "./ContactForm.module.css";
+
+interface ContactFormProps {
+    setShowHireForm: (value: boolean) => void;
+}
+const ContactForm:FC<ContactFormProps> = ({setShowHireForm}) => {
+    const email = "kirillmarchenko413@gmail.com";
+    const phone = "+420 792 448 178";
+    const additionalPhone = "+380 98 364 55 64";
+    return (
+        <form className={styles.form}>
+            <div className={styles.form_header}>Contact me</div>
+            <div className={styles.form_body}>
+                <div className={styles.form_block}>
+                    <span className={styles.form_label}>Email:</span>
+                    <span className={styles.form_value}>
+                        {email}
+                    </span>
+                </div>
+                <div className={styles.form_block}>
+                    <span className={styles.form_label}>Phone Number:</span>
+                    <span className={styles.form_value}>
+                        {phone}
+                    </span>
+                </div>
+                <div className={styles.form_block}>
+                    <span className={styles.form_label}>Additional Phone Number:</span>
+                    <span className={styles.form_value}>
+                        {additionalPhone}
+                    </span>
+                </div>
+                </div>
+            <div className={styles.form_footer}>
+                <div className={styles.form_buttons}>
+                    <button className={styles.form_button} onClick={(event) => {
+
+                        event.preventDefault();
+                    }}
+                    >Send
+                    </button>
+                    <button className={styles.form_button} onClick={(event) => {
+                        event.preventDefault();
+                        setShowHireForm(false)
+                    }}>Close
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default ContactForm;
