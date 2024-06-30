9d098e692410aefcda738ae86d4b00f4df60ee6c
MKO <118934540+SDT413@users.noreply.github.com>
2024-06-30 22:16:59 +0200

    changes

--- src/components/projects-section/ProjectsSection.tsx
+++ src/components/projects-section/ProjectsSection.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import Image from "next/image";
 import styles from "./ProjectsSection.module.css";
 import placeholder from "./assets/placeholder.jpg";
+import geography_minigames from "../../../public/assets/images/geography_minigames.png";
+import kitchen_chaos from "../../../public/assets/images/kitchen_chaos.png";
 
 import kids_cookies from "./assets/kids-cookies.png";
 import the_fba_machine from "./assets/thefbamachine.png";
 import coffee_shop from "./assets/coffee_shop.png";
 import spotify_clone from "./assets/spotify-clone.png";
 
-import GitHubSVG from "./assets/GitHubSVG";
-import ArrowInSquareSVG from "./assets/ArrowInSquareSVG";
 import { Observer } from "@/utils/observer";
-import Link from "next/link";
 import { ProjectData } from "@/components/interfaces/ProjectData";
 import ProjectElement from "./project-element/ProjectElement";
 
@@ -37,15 +35,6 @@ function ProjectsSection() {
       image: kids_cookies,
     },
     {
-      title: "Coffee shop",
-      description:
-        "",
-      technologies: ["React", "CSS"],
-      githubLink: "#",
-      liveDemoLink: "#",
-      image: coffee_shop,
-    },
-    {
       title: "Spotify clone",
       description:
         "Clone of spotify create in order to show my ability to work with next js, tailwind CSS and supabase",
@@ -67,7 +56,7 @@ function ProjectsSection() {
       ],
       githubLink: "#",
       liveDemoLink: "#",
-      image: placeholder,
+      image: coffee_shop,
     },
     {
       title: "GeographyMinigames",
@@ -84,18 +73,25 @@ function ProjectsSection() {
       ],
       githubLink: "#",
       liveDemoLink: "#",
-      image: placeholder,
+      image: geography_minigames,
     },
     {
       title: "Kitchen_Chaos",
       description:
-          "Massive Overcooked like game, the description for it would be: “Kitchen Chaos is a chaotic couch co-op cooking game for one to four players. Working as a team, you and your fellow chefs must prepare, cook and serve up a variety of tasty orders before the baying customers storm out in a huff. Sharpen your knives and dust off your chef’s whites, there isn’t mushroom for error and the steaks are high in these crazy kitchens!  The Burger Kingdom is in danger and only the finest cooking can save it! In Kitchen   Chaos  players must journey through a variety of cruel and unusual kitchens on their quest to become master chefs capable of conquering an ancient edible evil which plagues the land.Play solo or engage in classic, chaotic couch co-op for up to four players in both co-operative and competitive challenge modes. You’ll have to cook a range of different dishes and work together in order to become the most effective and ultimate team!”.The game uses netcode for game objects for multiplayer, Lobby and Relay services, and Unity Server. \n",
+          "Kitchen Chaos is a chaotic couch co-op cooking game for one to four players. " +
+          "Working as a team, you and your fellow chefs must prepare, cook and serve up a variety of tasty orders before the baying customers storm out in a huff." +
+          "Sharpen your knives and dust off your chef’s whites, there isn’t mushroom for error and the steaks are high in these crazy kitchens!" +
+          "The Burger Kingdom is in danger and only the finest cooking can save it! " +
+          "In Kitchen Chaos players must journey through a variety of cruel and unusual kitchens on their quest to become master chefs capable of conquering an ancient edible evil which plagues the land." +
+          "Play solo or engage in classic, chaotic couch co-op for up to four players in both co-operative and competitive challenge modes. " +
+          "You’ll have to cook a range of different dishes and work together in order to become the most effective and ultimate team!" +
+          "The game uses netcode for game objects for multiplayer, Lobby and Relay services, and Unity Server. \n",
       technologies: [
 
       ],
       githubLink: "#",
       liveDemoLink: "#",
-      image: placeholder,
+      image: kitchen_chaos,
     },
   ];
   return (
