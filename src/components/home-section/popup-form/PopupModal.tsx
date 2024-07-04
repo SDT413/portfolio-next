f316c830c6ba9c46683e697121538a51ea0446b2
MKO <118934540+SDT413@users.noreply.github.com>
2024-07-04 14:01:53 +0200

    ContactForm.module.css changed for deploy

--- src/components/home-section/popup-form/PopupModal.tsx
+++ src/components/home-section/popup-form/PopupModal.tsx
@@ -1,6 +1,5 @@
 import React, {FC} from 'react';
 import styles from "./PopupModal.module.css";
-import HireForm from "@/components/home-section/popup-form/hire-form/HireForm";
 
 interface PopupModalProps {
     children: React.ReactNode;
