0adac9748d6fb7386a8b303e9ee69e170ea9eb21
MKO <118934540+SDT413@users.noreply.github.com>
2024-07-04 14:25:57 +0200

    phone numbers in contacts section

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -4,6 +4,7 @@ import EnvelopeSVG from "./assets/EnvelopeSVG";
 import MapSVG from "./assets/MapSVG";
 import {useActions} from "@/hooks/useActions";
 import {Observer} from "@/utils/observer";
+import PhoneSVG from "@/components/contacts/assets/PhoneSVG";
 
 const Contacts: FC = ({}) => {
   const cbRef = Observer("contact");
@@ -19,17 +20,25 @@ const Contacts: FC = ({}) => {
         <span className={styles.contact_info}>
           <div className={styles.contact_info_wrapper}>
             <span className={styles.contact_icon_wrapper}>
-              <MapSVG className={styles.contact_icon} />
+              <MapSVG className={styles.contact_icon}/>
             </span>
             <h3 className={styles.contact_header}>Location</h3>
             <p className={styles.contact_paragraph}>Prague, Czech Republic</p>
           </div>
           <div className={styles.contact_info_wrapper} ref={cbRef}>
             <span className={styles.contact_icon_wrapper}>
-              <EnvelopeSVG className={styles.contact_icon} />
+              <EnvelopeSVG className={styles.contact_icon}/>
             </span>
             <h3 className={styles.contact_header}>Mail</h3>
             <p className={styles.contact_paragraph}>kiril309274@gmail.com</p>
+          </div>
+          <div className={styles.contact_info_wrapper}>
+            <span className={styles.contact_icon_wrapper}>
+              <PhoneSVG className={styles.contact_icon_white}/>
+            </span>
+            <h3 className={styles.contact_header}>Phone Number</h3>
+            <p className={styles.contact_paragraph}>+420 792 448 178</p>
+            <p className={styles.add_contact_paragraph}>+380 98 364 5564</p>
           </div>
         </span>
       </div>
